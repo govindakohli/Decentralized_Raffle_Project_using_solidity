50117707bb8c215c8a63ca98a054f15eebdd14e2
Govinda <mrkohli01@gmail.com>
2025-04-16 14:20:42 +0000

    Update

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -24,6 +24,9 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
+import {VRFConsumerBaseV2Plus} from "./lib\chainlink-brownie-contracts\contracts\src\v0.8\vrf\dev\VRFConsumerBaseV2Plus.sol";
+
+// import {VRFConsumerBaseV2Plus} from "@chainlink/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 
 /**
  * @title A Raffle contract
@@ -31,32 +34,62 @@ pragma solidity ^0.8.29;
  * @notice This contract is for creating raffle contract
  * @dev Implementing Chainlink VRFv2.5
  */
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
     // Errors //
     error Raffle__SendMoreToEnterRaffle();
 
     uint256 private immutable i_enteranceFee;
+    // @dev The duration of the lottery in seconds
+    uint256 private immutable i_interval;
     address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
 
     // Events //
 
     event RaffleEntered(address indexed player);
 
     // Constructor //
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 interval) {
         i_enteranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRuffle() public payable {
+    function enterRuffle() external payable {
         // require(msg.value >= i_enteranceFee, "Not Enough Amount sent!")
         require(msg.value >= i_enteranceFee, Raffle__SendMoreToEnterRaffle());
         s_players.push(payable(msg.sender));
         emit RaffleEntered(msg.sender);
     }
 
-    function pickWinner() public {}
+    //1. Get a random number
+    //2. Use random number to pick a player
+    //3. be automatically called
 
-    // getter functions
+    function pickWinner() external {
+        // Check to see if enough time has passed
+        // block.timestamp - s_lastTimeStamp > i_interval;
+        require(
+            block.timestamp - s_lastTimeStamp > i_interval,
+            "Unable to decide the Winner due to time issue"
+        );
+
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: s_keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
+    }
+
+    // getter functions //
 
     function getEntranceFee() external view returns (uint256) {
         return i_enteranceFee;
