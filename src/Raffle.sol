8a5395eaa80a5726b31fe6338c5f29cb088f24c7
Govinda <mrkohli01@gmail.com>
2025-04-26 13:26:57 +0000

    Latest Updates

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -24,9 +24,10 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
-import {VRFConsumerBaseV2Plus} from "./lib\chainlink-brownie-contracts\contracts\src\v0.8\vrf\dev\VRFConsumerBaseV2Plus.sol";
+// import {VRFConsumerBaseV2Plus} from "./lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 
-// import {VRFConsumerBaseV2Plus} from "@chainlink/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFConsumerBaseV2Plus} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
 /**
  * @title A Raffle contract
@@ -38,9 +39,14 @@ contract Raffle is VRFConsumerBaseV2Plus {
     // Errors //
     error Raffle__SendMoreToEnterRaffle();
 
+    uint16 private constant REQUEST_CONFIRMATION = 3;
+    uint32 private constant NUM_WORDS = 1;
     uint256 private immutable i_enteranceFee;
     // @dev The duration of the lottery in seconds
     uint256 private immutable i_interval;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
 
@@ -49,10 +55,21 @@ contract Raffle is VRFConsumerBaseV2Plus {
     event RaffleEntered(address indexed player);
 
     // Constructor //
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_enteranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        // s_vrfCoordinator.requestRandomWords();
     }
 
     function enterRuffle() external payable {
@@ -69,29 +86,38 @@ contract Raffle is VRFConsumerBaseV2Plus {
     function pickWinner() external {
         // Check to see if enough time has passed
         // block.timestamp - s_lastTimeStamp > i_interval;
-        require(
-            block.timestamp - s_lastTimeStamp > i_interval,
-            "Unable to decide the Winner due to time issue"
-        );
-
-        requestId = s_vrfCoordinator.requestRandomWords(
-            VRFV2PlusClient.RandomWordsRequest({
-                keyHash: s_keyHash,
-                subId: s_subscriptionId,
-                requestConfirmations: requestConfirmations,
-                callbackGasLimit: callbackGasLimit,
-                numWords: numWords,
+        // require(
+        //     block.timestamp - s_lastTimeStamp > i_interval,
+        //     "Unable to decide the Winner due to time issue"
+        // );
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient
+            .RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATION,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
                 extraArgs: VRFV2PlusClient._argsToBytes(
                     // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
                     VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
                 )
-            })
-        );
+            });
+        //   uint256  requestId = s_vrfCoordinator.requestRandomWords(
+        //  request
+        //     );
     }
 
-    // getter functions //
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal virtual override {}
 
-    function getEntranceFee() external view returns (uint256) {
-        return i_enteranceFee;
-    }
+    // // getter functions //
+
+    //     // function getEntranceFee() external view returns (uint256) {
+    //     //     return i_enteranceFee;
+    // }
 }
